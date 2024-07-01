ceacb50b606bf3eb95cb7b00192012f809d65983
Sean Theisen <seanjt829@gmail.com>
2024-06-30 23:37:42 -0400

    ML

---
+++ data_modeling/ml_py.py
@@ -0,0 +1,21 @@
+import sklearn
+
+# Supervised learning
+## Learning with labels
+## Naive Bayes
+## SVM
+## Random Decision Forest
+## Regression
+
+# Unsupervised
+## Clustering
+## Dimensionality Reduction
+
+# Reinforcement
+## Markov Decision Process
+## Approximate dynamic programming
+## Brute force
+
+# Recommender System
+## Col dSTart
+## Similarty Search
